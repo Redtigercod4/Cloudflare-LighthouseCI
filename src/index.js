78a20acff9e64cf368241860f1be3b91ab4c431b
Wesley Whittle <wesleywhittle@hotmail.co.uk>
2023-02-20 20:29:11 +0000

    Working server with issue on fetch api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+const {createServer} = require('@lhci/server');
+
+async function lighthouseServer () {
+	createServer({
+		port: 3000,
+		storage: {
+			storageMethod: 'sql',
+			sqlDialect: 'sqlite3',
+			sqlDatabasePath: env.binding,
+		},
+	}).then(({port}) => console.log('LHCI Listening on port', port));
+}
+
 export default {
-	
-};
+	async fetch (request) {
+		await lighthouseServer();
+	}
+}
